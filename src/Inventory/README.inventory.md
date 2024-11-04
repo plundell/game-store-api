ae7a4b351bb711b70e55bf477b66e124f4c9999e
Patrik <patrik@qmusicplayer.se>
2024-11-04 10:30:35 +0100

    Start creating inventory module

---
+++ src/Inventory/README.inventory.md
@@ -0,0 +1,20 @@
+# Inventory
+
+<table style="border-collapse: collapse; width: 80%;">
+    <tr>
+        <th>File</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td><a href="inventoryCrud.class.php">inventoryCrud.class.php</a></td>
+        <td>CRUD operations class for the <code>studios</code>, <code>games</code> and <code>ledger</code> tables</td>
+    </tr>
+    <tr>
+        <td><a href="inventory.settings.php">inventory.settings.php</a></td>
+        <td>Settings for all 3</td>
+    </tr>
+    <tr>
+        <td><a href="inventory.routes.php">inventory.routes.php</a></td>
+        <td>Routes for all 3</td>
+    </tr>
+</table>
---
+++ src/Inventory/invetory.routes.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Inventory;
+
+// require_once constant('BOOT_DIR') . '/RoutesFiles.interface.php';
+
+use App\Bootstrap\RoutesFilesInterface;
+use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+
+return new class implements RoutesFilesInterface {
+    public function __invoke(App $app): void
+    {
+        $app->group('/inventory', function (Group $group) {
+            $group->get('/{id}', function (Request $request, Response $response, array $args) {
+                $response->getBody()->write($args['id'] ?? 'i got nothing');
+                return $response;
+            });
+        });
+    }
+};
